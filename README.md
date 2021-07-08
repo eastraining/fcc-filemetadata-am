8a47fa6689a08fd969816b4248aea8fa2f7d0dda
Yu Dong Hee <heeyudong@gmail.com>
2021-07-08 13:16:38 +0000

    all tests passed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice)
+My implementation of this project. Try it out on Repl.it:
+
+[![Run on Repl.it](https://repl.it/badge/github/eastraining/fcc-filemetadata-am)](https://repl.it/github/eastraining/fcc-filemetadata-am)
--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const multer  = require('multer');
+const upload = multer();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
